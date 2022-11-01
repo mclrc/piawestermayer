0cbb4d7c47f49598a18a415f231212e02185e4bb
pixldemon <moritzamando@proton.me>
2022-11-01 22:05:12 +0100

    Add sitemap and meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
 	app: {
 		head: {
 			script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+			meta: [
+				{ name: 'description', content: 'Praxis von Dipl. Päd. Pia Westermayer, Kinder- und Jugendlichenpsychotherapie' },
+			]
 		},
 	},
 	modules: [
